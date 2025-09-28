4f71a48c791980cfac86324fb2a0d3c4e39c3d23
Kodirov2samir <kodirovsamir174@gmail.com>
2025-09-28 15:36:52 +0500

    made json and delete btn

--- src/conponents/content/Content.jsx
+++ src/conponents/content/Content.jsx
@@ -3,30 +3,40 @@ import { CiCircleChevLeft } from "react-icons/ci";
 import { CiCircleChevRight } from "react-icons/ci";
 import { IoSearch } from "react-icons/io5";
 import { CiBellOn } from "react-icons/ci";
-import { useState } from 'react';
+import { useState } from "react";
+import { FiTrash } from "react-icons/fi";
+import { GoPencil } from "react-icons/go";
+import axios from "axios";
+import { useEffect } from "react";
 function Content({ isOpen, setIsOpen }) {
-  const [name,addname] = useState("")
-  const [email,addemail] = useState("")
-  const [phone,addphone] = useState("")
-  const [enroll,addenroll] = useState("")
-  const [date,adddate] = useState("")
-  const [img,addimage] = useState("")
-  const [ovr,ovrArr]=useState([])
-  function handleSubmit(e) {
-    e.preventDefault()
-    let obj = {
-      name,
-      email,
-      phone,
-      enroll,
-      date,
-      img
-    }
-    ovrArr([...ovr,obj])
-  console.log(ovr);
-  
-  }
-  
+  const [name, addname] = useState("");
+  const [email, addemail] = useState("");
+  const [phone, addphone] = useState("");
+  const [enroll, addenroll] = useState("");
+  const [date, adddate] = useState("");
+  const [img, addimage] = useState("");
+  const [ovr, ovrArr] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/workers").then((res) => ovrArr(res.data));
+  }, []);
+
+  const postToBack = () => {
+    axios
+      .post("http://localhost:3001/workers", {
+        name,
+        email,
+        phone,
+        enroll,
+        date,
+        img,
+      })
+      .then((res) => ovrArr([...ovr, res.data]));
+  };
+  const deleteFromBack = (id) => {
+    axios
+      .delete(`http://localhost:3001/workers/${id}`)
+      .then(() => ovrArr(ovr.filter((item) => item.id !== id)));
+  };
   return (
     <main className="site-main">
       <header className="site-header">
@@ -59,37 +69,85 @@ function Content({ isOpen, setIsOpen }) {
       <section className="hero">
         <div className="hero-head">
           <h2>Student-List</h2>
-           <form onSubmit={handleSubmit} className='inp-wrap'>
-             <input onChange={(e) => addimage(e.target.value)} type="url" placeholder='image-link'/>
-            <input onChange={(e) => addname(e.target.value)} type="text" placeholder='name'/>
-            <input onChange={(e) => addemail(e.target.value)} type="email" placeholder='email'/>
-            <input onChange={(e) => addphone(e.target.value)} type="number" placeholder='Phone number'/>
-            <input onChange={(e) => addenroll(e.target.value)} type="number" placeholder='enroll number'/>
-            <input onChange={(e) => adddate(e.target.value)} type="date" placeholder='date-ofadmission'/>
-            <button className='btn'>Submit</button>
-           </form>
+          <form className="inp-wrap">
+            <input
+              onChange={(e) => addimage(e.target.value)}
+              type="url"
+              placeholder="image-link"
+            />
+            <input
+              onChange={(e) => addname(e.target.value)}
+              type="text"
+              placeholder="name"
+            />
+            <input
+              onChange={(e) => addemail(e.target.value)}
+              type="email"
+              placeholder="email"
+            />
+            <input
+              onChange={(e) => addphone(e.target.value)}
+              type="number"
+              placeholder="Phone number"
+            />
+            <input
+              onChange={(e) => addenroll(e.target.value)}
+              type="number"
+              placeholder="enroll number"
+            />
+            <input
+              onChange={(e) => adddate(e.target.value)}
+              type="date"
+              placeholder="date-ofadmission"
+            />
+            <button onClick={postToBack} className="btn">
+              Submit
+            </button>
+          </form>
         </div>
-        <thead>
-          <th className='names'>name</th>
-          <th>email</th>
-          <th>phone</th>
-          <th>enroll</th>
-          <th>date</th>
-        </thead>
-        <div className='hero-added'>  
-          {ovr.map((item,index)=>{
-            return(
-              <tbody className='user-info-wrap' key={index}>
-                <img className='user-image' src={item.img} alt="image of yours" />
-                <td className='user-name'>{item.name}</td>
-                <p className='user-email'>{item.email}</p>
-                <p className='user-phone'>{item.phone}</p>
-                <p className='user-enroll'>{item.enroll}</p>
-                <p className='user-date'>{item.date}</p>
-              </tbody>
-            )
-          })}   
-        </div>
+        <hr className="line" />
+        <table>
+          <thead>
+            <tr>
+              <th className="img-w"></th>
+              <th className="names">name</th>
+              <th>email</th>
+              <th>phone</th>
+              <th>enroll</th>
+              <th>date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody className="user-info-wrap">
+            {ovr.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <img
+                      className="user-image"
+                      src={item.img}
+                      alt="image of yours"
+                    />
+                  </td>
+                  <td className="user-name">{item.name}</td>
+                  <td className="user-email">{item.email}</td>
+                  <td className="user-phone">{item.phone}</td>
+                  <td className="user-enroll">{item.enroll}</td>
+                  <td className="user-date">{item.date}</td>
+                  <td>
+                    <span className="pencil">
+                      <GoPencil />
+                    </span>
+                    <span onClick={()=>deleteFromBack(item.id)}>
+                      <FiTrash />
+                    </span>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </section>
     </main>
   );
